43df7505a9173f25f5244277b10732fc26a68437
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-01-17 17:16:45 +0100

    Exercici 1

--- joke.ts
+++ joke.ts
@@ -1,1 +1,28 @@
-console.log('prueba');
+interface Joke {
+    joke: string;
+}
+
+
+document.addEventListener('DOMContentLoaded', getJoke);
+
+if (button) {
+    button.addEventListener('click', getJoke);
+}
+
+async function get(): Promise<void> {
+    try {
+        const jokeData = await fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        const jokeObj: Joke = await jokeData.json();
+
+        if (jokeText) {
+            jokeText.innerHTML = jokeObj.joke;
+        }
+    } catch (error) {
+        console.error('Error al obtener el chiste:', error);
+    }
+}
